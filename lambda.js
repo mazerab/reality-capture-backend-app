74e5b3b17feca7428142c32525606d2591071e3b
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-04-09 10:12:05 -0700

    Update lambda.js

--- lambda.js
+++ lambda.js
@@ -1,5 +1,14 @@
-'use strict'
+'use strict';
 const awsServerlessExpress = require('aws-serverless-express');
 const app = require('./app');
-const server = awsServerlessExpress.createServer(app);
+const binaryMimeTypes = [
+  'application/octet-stream',
+  'font/eot',
+  'font/opentype',
+  'font/otf',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
+];
+const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
 exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context);
