40b12cb6a9d334ddafd77279d0c6f8d5bd8dadce
Bastien Mazeran <bastien.mazeran@autodesk.com>
2018-04-12 09:53:04 -0700

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ module.exports = {
 	REALITY_CAPTURE_BASE_ENDPOINT: 'https://developer.api.autodesk.com/photo-to-3d/v1',
 	SCOPES: ['bucket:create','bucket:read','data:create','data:read','data:write'],
 	// Redis settings
-	REDIS_ENDPOINT: 'redis-19812.c15.us-east-1-2.ec2.cloud.redislabs.com',
+	REDIS_ENDPOINT: '<your redis endpoint>.cloud.redislabs.com',
 	REDIS_PORT: 19812,
 	// Amazon settings
-	AWS_LAMBDA_BASE_ENDPOINT: 'https://xikeyybzq8.execute-api.us-east-1.amazonaws.com/demo',
+	AWS_LAMBDA_BASE_ENDPOINT: 'https://<your lambda>.execute-api.us-east-1.amazonaws.com/demo',
 	// Autodesk Recap settings
 	// Available formats are rcm (Autodesk Recap Photo Mesh), 
 	// rcs (Autodesk Recap Point Cloud) and obj (Wavefront object).
